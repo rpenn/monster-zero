116ed45626b5f247b80490ed19ed5a86ab773aff
rpenn <ryanpenn00@gmail.com>
2015-11-11 09:35:57 -0500

    changed start script ahead of heroku deployment

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,9 +7,9 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         link: function (scope) {
 
             scope.items = [
-                { label: 'Home Yo!', state: 'home' },
+                { label: 'Home', state: 'home' },
                 { label: 'Bid now!', state: 'items' },
-                { label: 'Documentation', state: 'docs' },
+                { label: 'Add Items', state: 'add' },
                 { label: 'Members Only', state: 'membersOnly', auth: true }
             ];
 
